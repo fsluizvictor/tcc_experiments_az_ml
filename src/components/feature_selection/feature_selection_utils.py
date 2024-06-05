c508416ca9f2df067ba3288b3092340f4640f383
fsluizvictor <fs.luizvictor@gmail.com>
2024-06-05 02:11:10 +0000

    feat : add env variables

--- src/components/feature_selection/feature_selection_utils.py
+++ src/components/feature_selection/feature_selection_utils.py
@@ -11,3 +11,11 @@ def select_first_file(path) -> str:
     """
     files = os.listdir(path)
     return os.path.join(path, files[0])
+
+
+#ENVIROMENT VARIABLES
+PEARSON_FEAT_SEL = 'pearson'
+SPERMAN_FEAT_SEL = 'sperman'
+INFOGAIN_FEAT_SEL = 'infogain'
+GINI_FEAT_SEL = 'gini'
+FEATURE_KEY = 'feature'
