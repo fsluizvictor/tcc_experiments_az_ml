a710eda8590742b6ee82bd6089f854d55bd9dcdc
fsluizvictor <fs.luizvictor@gmail.com>
2024-06-05 03:34:27 +0000

    feat : improving notebooks

--- src/notebooks/pipelines/pipeline_utils.py
+++ src/notebooks/pipelines/pipeline_utils.py
@@ -31,3 +31,50 @@ PEARSON_PATH = "/home/azureuser/cloudfiles/code/Users/luiz.victor.dev/tcc_experi
 
 #PREP_DATA
 PREP_DATA_PATH = "/home/azureuser/cloudfiles/code/Users/luiz.victor.dev/tcc_experiments_az_ml/src/components/preparation/data_prep.yaml"
+
+#FEAT SEL
+GINI = 'Gini'
+INFOGAIN = 'Infogain'
+PEARSON = 'Pearson'
+SPERMAN = 'Sperman'
+
+#TRAIN
+GBC = 'GradientBoostingClassifier'
+NBC = 'GaussianNB'
+RFC = 'RandomForestClassifier'
+SVC = 'SVC'
+XGB = 'XGBoost'
+MODELS = [NBC, GBC, RFC, SVC, XGB]
+
+#PREPATION
+DATA_PREP = 'DataPreparation'
+
+#PIPELINES BY FEATURE SELECTION
+#INFOGAIN
+GBC_BY_INFOGAIN = f"{GBC}_BY_{INFOGAIN}"
+NBC_BY_INFOGAIN = f"{NBC}_BY_{INFOGAIN}"
+RFC_BY_INFOGAIN = f"{RFC}_BY_{INFOGAIN}"
+SVC_BY_INFOGAIN = f"{SVC}_BY_{INFOGAIN}"
+XGB_BY_INFOGAIN = f"{XGB}_BY_{INFOGAIN}"
+
+#GINI
+GBC_BY_GINI = f"{GBC}_BY_{GINI}"
+NBC_BY_GINI = f"{NBC}_BY_{GINI}"
+RFC_BY_GINI = f"{RFC}_BY_{GINI}"
+SVC_BY_GINI = f"{SVC}_BY_{GINI}"
+XGB_BY_GINI = f"{XGB}_BY_{GINI}"
+
+#SPERMAN
+GBC_BY_SPERMAN = f"{GBC}_BY_{SPERMAN}"
+NBC_BY_SPERMAN = f"{NBC}_BY_{SPERMAN}"
+RFC_BY_SPERMAN = f"{RFC}_BY_{SPERMAN}"
+SVC_BY_SPERMAN = f"{SVC}_BY_{SPERMAN}"
+XGB_BY_SPERMAN = f"{XGB}_BY_{SPERMAN}"
+
+#PEARSON
+GBC_BY_PEARSON = f"{GBC}_BY_{PEARSON}"
+NBC_BY_PEARSON = f"{NBC}_BY_{PEARSON}"
+RFC_BY_PEARSON = f"{RFC}_BY_{PEARSON}"
+SVC_BY_PEARSON = f"{SVC}_BY_{PEARSON}"
+XGB_BY_PEARSON = f"{XGB}_BY_{PEARSON}"
+
