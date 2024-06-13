b3e86132e9c63e076695cb2fc2a87b6459ed0fd7
fsluizvictor <fs.luizvictor@gmail.com>
2024-06-13 01:39:01 +0000

    feat : improving code

--- src/components/train/train_utils.py
+++ src/components/train/train_utils.py
@@ -41,29 +41,33 @@ def train_and_log_model(clf,
     _validate_inputs(X_train, X_test, y_train, y_test)
 
     print(f"Training with data of shape {X_train.shape}")
-    
-    with mlflow.start_run(run_name=model_name):
-        if model_name == "XGBoostClassifier":
-            mlflow.xgboost.autolog()
-        else:
+
+    if model_name != "XGBoostClassifier":
+        with mlflow.start_run(run_name=model_name):
             mlflow.sklearn.autolog()
 
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            return
+    
+    with mlflow.start_run(run_name=model_name):
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
 
-        if model_name != "XGBoostClassifier":
-            report = classification_report(y_test, y_pred)
-            print(report)
+        report = classification_report(y_test, y_pred)
+        print(report)
+            # Calculating metrics
+        accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
 
-        # Calculating metrics
-        #accuracy = accuracy_score(y_test, y_pred)
-        #f1 = f1_score(y_test, y_pred)
-        #precision = precision_score(y_test, y_pred)
-        #recall = recall_score(y_test, y_pred)
+        mlflow.log_metric('training_accuracy_score', accuracy)
+        mlflow.log_metric('f1_score', f1)
+        mlflow.log_metric('precision', precision)
+        mlflow.log_metric('recall', recall)
 
-        #print(f"Metrics:accuracy: {accuracy},f1: {f1},precision: {precision},recall: {recall}")
-
-        #_log_metrics(accuracy, f1, precision, recall)
+        print(f"Metrics:accuracy: {accuracy},f1: {f1},precision: {precision},recall: {recall}")
         
         mlflow.end_run()
 
@@ -83,20 +87,23 @@ def _validate_inputs(X_train, X_test, y_train, y_test):
     if len(X_train) != len(y_train) or len(X_test) != len(y_test):
         raise ValueError("Os tamanhos dos conjuntos de entrada e saída devem ser iguais.")
     
-def _log_metrics(accuracy, f1, precision, recall):
-    """Registra as métricas no MLflow.
-    
-    Args:
-        accuracy (float): Acurácia.
-        f1 (float): F1-score.
-        precision (float): Precisão.
-        recall (float): Recall.
-    """
-    mlflow.log_metric('accuracy', accuracy)
-    mlflow.log_metric('f1_score', f1)
-    mlflow.log_metric('precision', precision)
-    mlflow.log_metric('recall', recall)
+    mlflow.log_metric("num_samples_x_train", X_train.shape[0])
+    mlflow.log_metric("num_features_x_train", X_train.shape[1])
 
+    mlflow.log_metric("num_samples_y_train", y_train.shape[0])
+    mlflow.log_metric("num_features_y_train", y_train.shape[1])
+
+    mlflow.log_metric("num_samples_x_test", X_test.shape[0])
+    mlflow.log_metric("num_features_x_test", X_test.shape[1])
+
+    mlflow.log_metric("num_samples_y_test", y_test.shape[0])
+    mlflow.log_metric("num_features_y_test", y_test.shape[1])
+        
+    print("x_train", X_train)
+    print("y_train", y_train)
+    print("x_test", X_test)
+    print("y_test", y_test)
+    
 def _is_active():
     """Encerra o run ativo do MLflow, se houver."""
     if mlflow.active_run():
